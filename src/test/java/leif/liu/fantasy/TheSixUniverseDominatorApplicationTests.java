1f7d98dd5d47481c806ab4a355461c4ad157ecd5
Leif Liu <603529840@qq.com>
2018-10-18 10:08:09 +0800

    test

---
+++ src/test/java/leif/liu/fantasy/TheSixUniverseDominatorApplicationTests.java
@@ -0,0 +1,16 @@
+package leif.liu.fantasy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TheSixUniverseDominatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
