af327a480ae8d271925ce7c744bd5d2a7d6afd62
Leif Liu <603529840@qq.com>
2018-10-20 14:06:22 +0800

    导入commons.net包

--- src/main/java/leif/liu/fantasy/util/MyUtil.java
+++ src/main/java/leif/liu/fantasy/util/MyUtil.java
@@ -1,7 +1,13 @@
 package leif.liu.fantasy.util;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.springframework.web.multipart.MultipartFile;
 
 public class MyUtil {
     private MyUtil() {}
@@ -24,5 +30,35 @@ public class MyUtil {
 
     public static String getRankByScore(int score) {
         return score >= 80 ? "S" : score >= 60 ? "A" : score >= 40 ? "B" : score >= 20 ? "C" : "D";
+    }
+
+    public static void uploadFile(MultipartFile multipartFile, String fileName, String fTPPath) {
+        FTPClient fTPClient = new FTPClient();
+        InputStream inputStream = null;
+
+        try {
+            fTPClient.connect("127.0.0.1", 21);
+            fTPClient.login("Administrator", "lbc19951022");
+            boolean hasDirectory = fTPClient.changeWorkingDirectory(fTPPath);
+
+            if (!hasDirectory) {
+                fTPClient.makeDirectory(fTPPath);
+            }
+
+            fTPClient.changeWorkingDirectory(fTPPath);
+            fTPClient.setFileType(FTP.BINARY_FILE_TYPE);
+            inputStream = multipartFile.getInputStream();
+            fTPClient.storeFile(fileName, inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                inputStream.close();
+                fTPClient.logout();
+                fTPClient.disconnect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
